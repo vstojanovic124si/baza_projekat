2cba27dcec6b12c77f88158ec02f7120f56bd832
VeljkoStojanovic <veljkostojanovicima@gmail.com>
2026-05-30 14:58:29 +0200

    Final version 2.0

--- src/main/java/com/example/baze_podataka/controllers/SetOdlaganjeOtpadaValuesController.java
+++ src/main/java/com/example/baze_podataka/controllers/SetOdlaganjeOtpadaValuesController.java
@@ -18,17 +18,15 @@ public class SetOdlaganjeOtpadaValuesController {
     }
 
     public void runQuery(Connection connection) {
-        try{
-            createViewQuery(connection);
-
+        try {
             String query = "SELECT * FROM View_KriticanOtpadPoLokacijama ORDER BY Ukupna_Kolicina_Kg DESC;";
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
-            ObservableList<OdlaganjeOtpadaDto> result =  FXCollections.observableArrayList();
-            while(resultSet.next()){
-                String lokacija =  resultSet.getString("Lokacija_Odlaganja");
+            ObservableList<OdlaganjeOtpadaDto> result = FXCollections.observableArrayList();
+            while (resultSet.next()) {
+                String lokacija = resultSet.getString("Lokacija_Odlaganja");
                 int brojOdlaganja = resultSet.getInt("Broj_Odlaganja");
-                double ukupnaKolicina =  resultSet.getDouble("Ukupna_Kolicina_Kg");
+                double ukupnaKolicina = resultSet.getDouble("Ukupna_Kolicina_Kg");
                 int supstanca_id1 = resultSet.getInt("Supstanca_id");
                 String supstanca_naziv = resultSet.getString("Supstanca_naziv");
 
@@ -36,31 +34,7 @@ public class SetOdlaganjeOtpadaValuesController {
                 result.add(odlaganjeOtpadaDto);
             }
             tvOdlaganjeOtpada.setItems(result);
-        }
-        catch (Exception e){
-
-        }
-    }
-
-    private void createViewQuery(Connection connection) {
-        try{
-            String query = "CREATE OR REPLACE VIEW View_KriticanOtpadPoLokacijama AS\n" +
-                    "SELECT\n" +
-                    "oo.lokacija AS Lokacija_Odlaganja,\n" +
-                    "COUNT(oo.otpad_id) AS Broj_Odlaganja,\n" +
-                    "CAST(SUM(ho.kolicina) AS DECIMAL(10,2)) AS Ukupna_Kolicina_Kg,\n" +
-                    "s.supstanca_id as Supstanca_id, s.supstanca_naziv as Supstanca_naziv\n" +
-                    "FROM odlaganje_otpada oo\n" +
-                    "\t\t\t\tJOIN hemijski_otpad ho ON oo.otpad_id = ho.otpad_id\n" +
-                    "\t\t\t\tJOIN hemijska_supstanca s ON ho.supstanca_id = s.supstanca_id\n" +
-                    "\t\t\t\tWHERE oo.bezbednosni_nivo <> 'Nizak'\n" +
-                    "\t\t\t\tGROUP BY oo.lokacija, s.supstanca_id, s.supstanca_naziv\n" +
-                    "\t\t\t\tHAVING SUM(ho.kolicina) > 1.00;";
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(query);
-        }
-        catch (Exception e){
-            System.out.println(e.getMessage());
+        } catch (Exception e) {
         }
     }
 }
